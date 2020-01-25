305d805ea8bf77ea63ebfb30798f56e57756d9c1
Lorcan O'Donoghue <LorcanODonoghue@outlook.com>
2020-01-25 19:39:22 +0000

    Setup Grpc api contracts

---
+++ MyCompanyPlayground/Services/CompanyService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using MyCompanyPlayground.Protos;
+
+namespace MyCompanyPlayground.Services
+{
+	public class CompanyService : CompanyServices.CompanyServicesBase
+	{
+		private readonly ILogger<CompanyService> _logger;
+
+		public CompanyService(ILogger<CompanyService> logger)
+		{
+			_logger = logger;
+		}
+		
+		public override Task<Response> AddCompany(Company request, ServerCallContext context)
+		{
+			return Task.FromResult(new Response
+			{
+				Status = "Hello",
+				ErrorMessage = string.Empty
+			});
+		}
+	}
+}

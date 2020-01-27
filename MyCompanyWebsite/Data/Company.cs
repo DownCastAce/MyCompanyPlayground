4176e5fe50b24dcf92ea7ecc266b9abf047a4665
Lorcan O'Donoghue <LorcanODonoghue@outlook.com>
2020-01-27 23:58:54 +0000

    Adding GUI for all API calls except UpdateCompany

---
+++ MyCompanyWebsite/Data/Company.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompanyWebsite.Data
+{
+    public class Company
+    {
+        /// <summary>
+        /// Name of the Company
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Stock Exchange
+        /// </summary>
+        [Required]
+        public string Exchange { get; set; }
+
+        /// <summary>
+        /// Stock Ticker
+        /// </summary>
+        [Required]
+        public string Ticker { get; set; }
+
+        /// <summary>
+        /// International Securities Identification Numbers
+        /// </summary>
+        [Required]
+        public string Isin { get; set; }
+
+        /// <summary>
+        /// Company Website
+        /// </summary>
+        public string Website { get; set; }
+    }
+}
---
+++ MyCompanyWebsite/Proxies/MyCompanyClient.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Grpc.Core;
+using Grpc.Net.Client;
+using MyCompanyPlayground.Protos;
+using MyCompanyWebsite.Data;
+
+namespace MyCompanyWebsite.Proxies
+{
+	public class MyCompanyClient
+	{
+		private CompanyServices.CompanyServicesClient _client; 
+		
+		public MyCompanyClient()
+		{
+			var channel = GrpcChannel.ForAddress("https://localhost:5001");
+			_client = new CompanyServices.CompanyServicesClient(channel);
+		}
+
+		public IEnumerable<Company> GetAllCompanies()
+		{
+			var response = _client.GetAllCompanies(new GetAllCompaniesRequest());
+
+			foreach(var company in response.Company)
+			{
+				yield return new Company
+				{
+					Name = company.CompanyName,
+					Exchange = company.Exchange,
+					Ticker = company.Ticker,
+					Isin = company.Isin,
+					Website = company.Website
+				};
+			}
+		}
+
+		public Company FindCompanyByIsin(string isin)
+		{
+			var request = new GetCompanyByIsinRequest 
+			{
+				Isin = isin
+			};
+			
+			var response = _client.GetCompanyByIsin(request);
+
+			return new Company 
+			{
+				Name = response.Company.CompanyName,
+				Exchange = response.Company.CompanyName,
+				Isin = response.Company.Isin,
+				Ticker = response.Company.Ticker,
+				Website = response.Company.Website
+			};
+
+		}
+
+		public Company FindCompanyById(int id)
+		{
+			var request = new GetCompanyByIdRequest
+			{
+				Id = id
+			};
+
+			var response = _client.GetCompanyById(request);
+
+			return new Company
+			{
+				Name = response.Company.CompanyName,
+				Exchange = response.Company.CompanyName,
+				Isin = response.Company.Isin,
+				Ticker = response.Company.Ticker,
+				Website = response.Company.Website
+			};
+
+		}
+
+		public string AddCompany(Company company)
+		{
+			AddCompanyRequest request = new AddCompanyRequest 
+			{
+				Company = new CompanyPayload
+				{
+					CompanyName = company.Name,
+					Exchange = company.Exchange,
+					Ticker = company.Ticker,
+					Isin = company.Isin,
+					Website = company.Website ?? string.Empty
+				}
+			};
+			try
+			{
+				var response = _client.AddCompany(request);
+				return response.Message;
+			}
+			catch (RpcException exception)
+			{
+				return exception.Status.Detail;
+			}
+		}
+	}
+}
